3ca297d6ac4458424cd2d7edbb522f5728ccdd8e
Morten Fjeldstad <morten@hubii.com>
2018-09-06 16:58:54 +0200

    Add documentation for Receipt and utils

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 const StriimProvider = require('./lib/striim-provider');
 const Wallet = require('./lib/wallet');
 const Payment = require('./lib/payment');
+const Receipt = require('./lib/receipt');
 const utils = require('./lib/utils');
 
 module.exports = {
     StriimProvider,
     Wallet,
     Payment,
+    Receipt,
     utils
 };
