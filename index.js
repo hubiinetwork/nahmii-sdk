dce3cea4dd6191bb9151b598466ea7b20dc64bac
Morten Fjeldstad <morten@hubii.com>
2019-05-03 09:37:49 +0200

    Renamed exports: - DriipSettlementChallenge -> DriipSettlement - NullSettlementChallenge -> NullSettlement
    This aligns naming in code, docs and exports.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Wallet = require('./lib/wallet');
 const Payment = require('./lib/payment');
 const Receipt = require('./lib/receipt');
 const MonetaryAmount = require('./lib/monetary-amount');
-const {Settlement, DriipSettlementChallenge, NullSettlementChallenge} = require('./lib/settlement');
+const {Settlement, DriipSettlement, NullSettlement} = require('./lib/settlement');
 const utils = require('./lib/utils');
 const Erc20Contract = require('./lib/erc20/erc20-contract');
 const NahmiiContract = require('./lib/contract');
@@ -19,8 +19,8 @@ module.exports = {
     Payment,
     Receipt,
     MonetaryAmount,
-    DriipSettlementChallenge,
-    NullSettlementChallenge,
+    DriipSettlement,
+    NullSettlement,
     Settlement,
     Erc20Contract,
     NahmiiContract,
