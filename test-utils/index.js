1f5aa657486de45f75e483320675146010699c2b
Morten Fjeldstad <morten@hubii.com>
2019-05-15 12:23:12 +0200

    Extract factory for payment and receipt JSON payloads

---
+++ test-utils/index.js
@@ -0,0 +1,222 @@
+'use strict';
+
+const ethers = require('ethers');
+const {privateToAddress} = require('ethereumjs-util');
+const {hash, prefix0x, fromRpcSig} = require('../lib/utils');
+const proxyquire = require('proxyquire').noPreserveCache().noCallThru();
+
+const Wallet = proxyquire('../lib/wallet/wallet', {
+    './client-fund-contract'    : function() {
+        return {};
+    },
+    './balance-tracker-contract': function() {
+        return {};
+    },
+    './erc20-contract'          : function() {
+        return {};
+    }
+});
+
+class ApiPayloadFactory {
+    constructor(options) {
+        this.amount = options.amount;
+        this.currency = options.currency;
+
+        this.senderPrivateKey = options.senderPrivateKey;
+        this.sender = options.sender || this.senderPrivateKey ?
+            prefix0x(privateToAddress(Buffer.from(this.senderPrivateKey, 'hex')).toString('hex')) :
+            undefined;
+        this.recipient = options.recipient;
+        this.operatorPrivateKey = options.operatorPrivateKey;
+        this.operator = options.operator || this.operatorPrivateKey ?
+            prefix0x(privateToAddress(Buffer.from(this.operatorPrivateKey, 'hex')).toString('hex')) :
+            undefined;
+    }
+
+    createUnsignedPayment(senderRef) {
+        const senderData = Buffer
+            .from(JSON.stringify({ref: senderRef}))
+            .toString('base64');
+
+        return {
+            amount   : this.amount,
+            currency : this.currency,
+            sender   : {
+                wallet: this.sender,
+                data  : senderData
+            },
+            recipient: {
+                wallet: this.recipient
+            }
+        };
+    }
+
+    async createSignedPayment(unsignedPayment) {
+        const signedPayment = {
+            ...unsignedPayment,
+            seals: {
+                wallet: {
+                    hash: ApiPayloadFactory.hashPayment(unsignedPayment)
+                }
+            }
+        };
+        signedPayment.seals.wallet.signature = await this.signAsSender(signedPayment.seals.wallet.hash);
+        return signedPayment;
+    }
+
+    createUnsignedReceipt(signedPayment) {
+        return {
+            ...signedPayment,
+            nonce      : 1,
+            blockNumber: 1,
+            operatorId : 1,
+            sender     : {
+                ...signedPayment.sender,
+                nonce   : 1,
+                balances: {
+                    current : '0',
+                    previous: '100'
+                },
+                fees    : {
+                    single: {
+                        currency: this.currency,
+                        amount  : '1000000000000000000'
+                    },
+                    total : [
+                        {
+                            originId: '1',
+                            figure  : {
+                                currency: this.currency,
+                                amount  : '1000000000000000000'
+                            }
+                        }
+                    ]
+                }
+            },
+            recipient  : {
+                ...signedPayment.recipient,
+                nonce   : 1,
+                balances: {
+                    current : '100',
+                    previous: '0'
+                },
+                fees    : {
+                    total: [
+                        {
+                            originId: '1',
+                            figure  : {
+                                currency: {
+                                    ct: '0x0000000000000000000000000000000000000001',
+                                    id: '1'
+                                },
+                                amount  : '1000000000000000000'
+                            }
+                        }
+                    ]
+                }
+            },
+            transfers  : {
+                single: '100',
+                total : '100'
+            }
+        };
+    }
+
+    async createSignedReceipt(unsignedReceipt) {
+        let signedReceipt = {
+            ...unsignedReceipt,
+            seals: {
+                ...unsignedReceipt.seals,
+                operator: {
+                    hash: ApiPayloadFactory.hashReceipt(unsignedReceipt)
+                }
+            }
+        };
+        signedReceipt.seals.operator.signature = await this.signAsOperator(signedReceipt.seals.operator.hash);
+        return signedReceipt;
+    }
+
+    static hashPayment(unsignedPayment) {
+        const amountCurrencyHash = hash(
+            unsignedPayment.amount,
+            unsignedPayment.currency.ct,
+            unsignedPayment.currency.id
+        );
+        const senderHash = hash(
+            unsignedPayment.sender.wallet,
+            unsignedPayment.sender.data
+        );
+        const recipientHash = hash(
+            unsignedPayment.recipient.wallet
+        );
+
+        return hash(amountCurrencyHash, senderHash, recipientHash);
+    }
+
+    static hashReceipt(unsignedReceipt) {
+        const walletSignatureHash = hash(
+            {type: 'uint8', value: unsignedReceipt.seals.wallet.signature.v},
+            unsignedReceipt.seals.wallet.signature.r,
+            unsignedReceipt.seals.wallet.signature.s
+        );
+        const nonceHash = hash(
+            unsignedReceipt.nonce
+        );
+        const senderHash = hash(
+            hash(unsignedReceipt.sender.nonce),
+            hash(
+                unsignedReceipt.sender.balances.current,
+                unsignedReceipt.sender.balances.previous
+            ),
+            hash(
+                unsignedReceipt.sender.fees.single.amount,
+                unsignedReceipt.sender.fees.single.currency.ct,
+                unsignedReceipt.sender.fees.single.currency.id
+            ),
+            ApiPayloadFactory.hashFeesTotal(unsignedReceipt.sender.fees.total)
+        );
+        const recipientHash = hash(
+            hash(unsignedReceipt.recipient.nonce),
+            hash(
+                unsignedReceipt.recipient.balances.current,
+                unsignedReceipt.recipient.balances.previous
+            ),
+            ApiPayloadFactory.hashFeesTotal(unsignedReceipt.recipient.fees.total)
+        );
+        const transfersHash = hash(
+            unsignedReceipt.transfers.single,
+            unsignedReceipt.transfers.total
+        );
+        return hash(walletSignatureHash, nonceHash, senderHash, recipientHash, transfersHash);
+    }
+
+    static hashFeesTotal(totalFigures) {
+        let feesTotalHash = 0;
+        for (let i = 0; i < totalFigures.length; i++) {
+            feesTotalHash = hash(
+                feesTotalHash,
+                totalFigures[i].originId,
+                totalFigures[i].figure.amount,
+                totalFigures[i].figure.currency.ct,
+                totalFigures[i].figure.currency.id
+            );
+        }
+        return feesTotalHash;
+    }
+
+    async signAsSender(hash) {
+        const senderWallet = new Wallet(this.senderPrivateKey);
+        const rpcSignature = await senderWallet.signMessage(ethers.utils.arrayify(hash));
+        return fromRpcSig(rpcSignature);
+    }
+
+    async signAsOperator(hash) {
+        const operatorWallet = new Wallet(this.operatorPrivateKey);
+        const rpcSignature = await operatorWallet.signMessage(ethers.utils.arrayify(hash));
+        return fromRpcSig(rpcSignature);
+    }
+}
+
+module.exports = {
+    ApiPayloadFactory
+};
