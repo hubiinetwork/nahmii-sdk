250e2bdf76815990435400d2b614790c5a854378
kata <kata.choi@gmail.com>
2019-10-31 14:40:40 +0800

    Regenerate docs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Payment = require('./lib/payment');
 const Receipt = require('./lib/receipt');
 const MonetaryAmount = require('./lib/monetary-amount');
 const Currency = require('./lib/currency');
-const {SettlementFactory} = require('./lib/settlement');
+const {SettlementFactory, PaymentSettlement, OnchainBalanceSettlement} = require('./lib/settlement');
 const utils = require('./lib/utils');
 const Erc20Contract = require('./lib/erc20/erc20-contract');
 const NahmiiContract = require('./lib/contract');
@@ -23,6 +23,8 @@ module.exports = {
     MonetaryAmount,
     SettlementFactory,
     Currency,
+    PaymentSettlement,
+    OnchainBalanceSettlement,
     Erc20Contract,
     NahmiiContract,
     InsufficientFundsError,
